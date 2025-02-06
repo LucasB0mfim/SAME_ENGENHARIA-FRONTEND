123933ae8d30714190fb4bd071f7309a672969b5
lucas <lucasbomfimsobral@gmail.com>
2025-02-06 07:51:09 -0300

    fix: Corrigir banner

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,11 +9,4 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
   logo: string = 'logo.png';
-  // videoUrl: string = 'https://github.com/LucasB0mfim/SAME_ENGENHARIA-VIDEO/raw/refs/heads/main/same.mp4';
-  videoUrl: string = 'banner.mp4';
-  isVideoLoaded: boolean = false;
-
-  onVideoLoad() {
-    this.isVideoLoaded = true;
-  }
 }
