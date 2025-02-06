6fba11af2f817f69ba5686723257b59fcf20e4b4
lucas <lucasbomfimsobral@gmail.com>
2025-02-05 22:36:16 -0300

    fix: Trocar url do banner

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,7 +9,8 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
   logo: string = 'logo.png';
-  videoUrl: string = 'https://github.com/LucasB0mfim/SAME_ENGENHARIA-VIDEO/raw/refs/heads/main/same.mp4';
+  // videoUrl: string = 'https://github.com/LucasB0mfim/SAME_ENGENHARIA-VIDEO/raw/refs/heads/main/same.mp4';
+  videoUrl: string = 'banner.mp4';
   isVideoLoaded: boolean = false;
 
   onVideoLoad() {
