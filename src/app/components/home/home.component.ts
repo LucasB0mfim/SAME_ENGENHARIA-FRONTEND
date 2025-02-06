2b0bfe0321d9aa7e031f90136d7a2f259228400e
lucas <lucasbomfimsobral@gmail.com>
2025-02-05 21:14:09 -0300

    Feat: Trocar vídeo do banner

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,43 +7,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-  wallpaper: string = 'wallpaper.jpg';
+export class HomeComponent {
   logo: string = 'logo.png';
-  video: string = 'same.mp4';
+  videoUrl: string = 'https://github.com/LucasB0mfim/SAME_ENGENHARIA-VIDEO/raw/refs/heads/main/same.mp4';
   isVideoLoaded: boolean = false;
 
-  ngOnInit() {
-    // Verifica se o dispositivo é mobile
-    const isMobile = window.innerWidth <= 768;
-
-    // Inicia o carregamento do vídeo apenas se não for mobile
-    if (!isMobile) {
-      this.loadVideo();
-    }
-  }
-
-  private loadVideo() {
-    // Carrega o vídeo usando Intersection Observer
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.1
-    };
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          this.isVideoLoaded = true;
-          observer.disconnect();
-        }
-      });
-    }, options);
-
-    // Observa a seção do banner
-    const bannerElement = document.querySelector('.banner');
-    if (bannerElement) {
-      observer.observe(bannerElement);
-    }
+  onVideoLoad() {
+    this.isVideoLoaded = true;
   }
 }
