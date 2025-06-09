9cbadc8b1bee9d07b13d70807eedfbf8845d466d
LucasB0mfim <lucasbomfimsobral@gmail.com>
2025-06-09 14:59:39 -0300

    feat: Refatorar header do home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from "../../components/public/footer/footer.component";
-import { HeaderComponent } from "../../components/public/header/header.component";
-import { WhatsappBotaoComponent } from "../../components/public/whatsapp-botao/whatsapp-botao.component";
-import { BannerComponent } from "../../components/public/banner/banner.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, FooterComponent, HeaderComponent, WhatsappBotaoComponent, BannerComponent],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  
+  logo: string = 'assets/images/logo-orange.png'
+
+  menuAtivo = false;
+
+  toggleMenu() {
+    this.menuAtivo = !this.menuAtivo;
+  }
 }
