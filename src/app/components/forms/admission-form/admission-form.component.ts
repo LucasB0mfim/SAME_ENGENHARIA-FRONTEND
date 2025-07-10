5a81da67cad042f08aa97c2a3c048596ddbc22dd
lucas <lucasbomfimsobral@gmail.com>
2025-07-10 16:21:59 -0300

    Style: Alterar layout do formulário

--- src/app/components/forms/admission-form/admission-form.component.ts
+++ src/app/components/forms/admission-form/admission-form.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-admission-form',
-  imports: [NgxMaskDirective, NgxMaskPipe],
+  imports: [NgxMaskDirective],
   providers: [provideNgxMask()],
   templateUrl: './admission-form.component.html',
   styleUrl: './admission-form.component.scss'
