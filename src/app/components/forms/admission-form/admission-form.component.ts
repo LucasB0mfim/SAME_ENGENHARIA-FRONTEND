4888069f6a0423ddc2fdc17f2fc43ae303a101f0
LucasB0mfim <lucasbomfimsobral@gmail.com>
2025-07-14 15:17:29 -0300

    feat: Adicionar identificador de campos do formulário

--- src/app/components/forms/admission-form/admission-form.component.ts
+++ src/app/components/forms/admission-form/admission-form.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admission-form',
-  imports: [NgxMaskDirective, MatIconModule],
+  imports: [NgxMaskDirective, MatIconModule, CommonModule, ReactiveFormsModule],
   providers: [provideNgxMask()],
   templateUrl: './admission-form.component.html',
   styleUrl: './admission-form.component.scss'
@@ -13,8 +15,82 @@ export class AdmissionFormComponent {
 
   // ========== ESTADOS ==========
   useVT: string = '';
-  hasChildren: string = '';
   useBoot: string = '';
+  hasChildren: string = '';
+  hasChronicCondition: string = '';
+  children: string[] = [];
+
+  selectedFunction: string = '';
+  needBoot: string[] = [
+    'PINTOR',
+    'SERVENTE',
+    'PEDREIRO',
+    'ARMADOR',
+    'SOLDADOR',
+    'BETONEIRO',
+    'ELETRICISTA',
+    'ALMOXARIFE',
+    'ENGENHEIRO',
+    'ENCANADOR',
+    'SERRALHEIRO',
+    'CARPINTEIRO',
+    'ENCARREGADO',
+    'APONTADOR',
+    'MESTRE DE OBRAS',
+    'AUXILIAR DE ENGENHARIA',
+    'TÉCNICO DE SEGURANÇA',
+    'TÉCNICO EM EDIFICAÇÕES',
+    'OPERADOR DE MÁQUINAS',
+    'MOTORISTA DE CAMINHÃO',
+  ];
+
+  hapvida: string[] = [
+    'ADMINISTRATIVO',
+    'ENGENHEIRO',
+    'TÉCNICO DE SEGURANÇA',
+    'TÉCNICO EM EDIFICAÇÕES'
+  ];
+
+  admissionForm: FormGroup = new FormGroup({
+    nome: new FormControl(''),
+    cpf: new FormControl(''),
+    dataNascimento: new FormControl(''),
+    numeroContato: new FormControl(''),
+    rg: new FormControl(''),
+    pis: new FormControl(''),
+    endereco: new FormControl(''),
+    complemento: new FormControl(''),
+    cep: new FormControl(''),
+    tamanhoFarda: new FormControl(''),
+    usaVt: new FormControl(''),
+    quantidadeVT: new FormControl(''),
+    funcao: new FormControl(''),
+    tamanhoBota: new FormControl(''),
+    hapvida: new FormControl(''),
+    possuiFilho: new FormControl(''),
+    nomeFilho: new FormControl(''),
+    cpfFilho: new FormControl(''),
+    cpfFilhoFoto: new FormControl(''),
+    certidaoNascimentoFilho: new FormControl(''),
+    instagram: new FormControl(''),
+    linkedin: new FormControl(''),
+    tipoSanguineo: new FormControl(''),
+    contatoEmergencia: new FormControl(''),
+    grauParentesco: new FormControl(''),
+    numeroContatoEmergencia: new FormControl(''),
+    contatoEmergenciaSecundario: new FormControl(''),
+    grauParentescoSecundario: new FormControl(''),
+    numeroContatoEmergenciaSecundario: new FormControl(''),
+    alergia: new FormControl(''),
+    condicaoMedica: new FormControl(''),
+    tipoCondicaoMedica: new FormControl(''),
+    foto3x4: new FormControl(''),
+    cpfFoto: new FormControl(''),
+    rgFrente: new FormControl(''),
+    rgVerso: new FormControl(''),
+    certidaoNascimento: new FormControl(''),
+    comproventeResidencia: new FormControl('')
+  })
 
   // ========== UTILITÁRIOS ==========
   onManageVT(event: Event): void {
@@ -30,5 +106,28 @@ export class AdmissionFormComponent {
   onManageBoot(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
     this.useBoot = selectElement.value;
+  }
+
+  onManageChronicCondition(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    this.hasChronicCondition = selectElement.value;
+  }
+
+  onFuncaoChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.selectedFunction = target.value;
+  }
+
+  addChildren(): void {
+    this.children.push('');
+  }
+
+  updateChildren(index: number, event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.children[index] = target.value;
+  }
+
+  removeChildren(index: number): void {
+    this.children.splice(index, 1);
   }
 }
